1723717eb998f85edf607fd075d2e390db111870
Ladinu Chandrasinghe <ladinu.chandrasinghe@oracle.com>
2021-05-10 09:23:22 -0700

    Started work on ui

--- src/js/deploy_web3.js
+++ src/js/deploy_web3.js
@@ -2,33 +2,67 @@
 (async () => {
     try {
         console.log('Running deployWithWeb3 script...')
-        
-        const contractName = 'Storage' // Change this for other contract
-        const constructorArgs = []    // Put constructor args (if any) here for your contract
-    
-        // Note that the script needs the ABI which is generated from the compilation artifact.
-        // Make sure contract is compiled and artifacts are generated
-        const artifactsPath = `browser/contracts/artifacts/${contractName}.json` // Change this for different path
 
 
         const metadata = await fetch('contracts/artifacts/FFContract_metadata.json')
             .then(response => response.json())
 
-        const accounts = await ethereum.request({ method: 'eth_requestAccounts' });  
+        const account = await ethereum
+            .request({ method: 'eth_requestAccounts' })
+            .then(accounts => accounts[0]);
     
-        let contract = ethereum.contract(metadata.abi)
+        window.commissioner = null;
+        window.players = [];
+
+        window.deployContract = () => {
+            console.log("working");
+        };
+
+        window.addPlayer = (player) => {
+            players.push({
+                name: $('#playerName').val(),
+                addr: $('#playerWallet').val()
+            });
+            updatePlayerView();
+        };
+
+        removePlayer = (index) => {
+            players = players.filter((_, i) => i != index);
+            updatePlayerView();
+        };
+
+        updatePlayerView = () => {
+            $('#player-list').empty();
+
+            players.forEach((player, index) => {
+                $('#player-list').append(`
+                            <li class="list-group-item">
+                                ${player.name}
+                                <div id="" class="form-text">${player.addr}</div>
+                                <svg onClick="removePlayer(${index});" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                </svg>
+                            </li>
+                `);
+            });
+        };
+
+
+
+        // let contract = ethereum.contract(metadata.abi)
     
-        contract = contract.deploy({
-            data: metadata.data.bytecode.object,
-            arguments: constructorArgs
-        })
+        // contract = contract.deploy({
+        //     data: metadata.data.bytecode.object,
+        //     arguments: constructorArgs
+        // })
     
-        const newContractInstance = await contract.send({
-            from: accounts[0],
-            gas: 1500000,
-            gasPrice: '30000000000'
-        })
-        console.log('Contract deployed at address: ', newContractInstance.options.address)
+        // const newContractInstance = await contract.send({
+        //     from: accounts[0],
+        //     gas: 1500000,
+        //     gasPrice: '30000000000'
+        // })
+        // console.log('Contract deployed at address: ', newContractInstance.options.address)
     } catch (e) {
         console.log(e.message)
     }
